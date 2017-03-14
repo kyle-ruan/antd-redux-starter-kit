03cce555f14abeee895507717ce871e98414643d
Kyle-Ruan <kyleryk123@gmail.com>
2017-03-14 19:57:13 +1100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { DatePicker } from 'antd';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <DatePicker />
+      </div>
+    );
+  }
+}
+
+export default App;
