2c2abf4bdeb48eb2139b395d994b26d06d65d8cd
Phillip McCallum <phillip@trueagency.com.au>
2017-03-21 11:19:02 +1100

    Test pull request

---
+++ src/components/CoreButton.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Button } from 'antd';
+import FaIcon from "./FaIcon.js";
+
+class CoreButton extends Button {
+    constructor(props) {
+        super(props);
+
+        var icon = '';
+        var text = '';
+
+        if(this.props.icon !== undefined) {
+            icon = <FaIcon type={this.props.icon} />;
+        }
+
+        if(this.props.children !== undefined) {
+            text = this.props.children;
+        }
+
+        this.state = {
+            text: text,
+            icon: icon
+        };
+
+    }
+  
+    render() {
+        return (<Button type={this.props.type} size={this.props.size}>
+            {this.state.icon}<span style={ this.state.text === '' ? {display: 'none'} : {} }>{ this.state.text}</span>
+        </Button>)
+    }
+}
+
+export default CoreButton;
